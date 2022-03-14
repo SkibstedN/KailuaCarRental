7a72a9f24bf5afba2106c6138666f324a742b4d0
SkibstedN <niko933n@stud.kea.dk>
2022-03-14 14:30:12 +0100

    Oprettet lister i ListContainer

--- src/com/company/ListContainer.java
+++ src/com/company/ListContainer.java
@@ -1,4 +1,10 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class ListContainer {
+    ArrayList<FamilyCar> familyCars = new ArrayList<>();
+    ArrayList<LuxuryCar> luxuryCars = new ArrayList<>();
+    ArrayList<SportsCar> sportsCars = new ArrayList<>();
+    ArrayList<Contract> contracts = new ArrayList<>();
 }
