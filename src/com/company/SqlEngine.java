1094c78edef016aeec551234550098c401ef78b0
SkibstedN <niko933n@stud.kea.dk>
2022-03-14 15:07:17 +0100

    Updated SqlEngine

--- src/com/company/SqlEngine.java
+++ src/com/company/SqlEngine.java
@@ -1,4 +1,29 @@
 package com.company;
 
+import java.sql.*;
+
 public class SqlEngine {
+
+    public static final String jdbc_driver = "com.mysql.jdbc.Driver";
+    public static final String database_url = "jdbc:mysql://localhost:3306/ap";
+    public static Connection con;
+
+    public static void main(String[] args) {
+        try{
+            con = DriverManager.getConnection(database_url, "root", "");
+            Statement s = con.createStatement();
+            ResultSet rs = s.executeQuery("/*SQL STATEMENT HERE*/");
+
+            if (rs != null){
+                while (rs.next()){
+                    /*System.out.print("something,something");*/
+                }
+            }
+            s.close();
+            con.close();
+        }catch(SQLException sqlException){
+            System.out.println("SqlException: "+sqlException.getMessage());
+            System.exit(1);
+        }
+    }
 }
