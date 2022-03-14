529548965c4d23e5ca9b8a987811757cc597d8fc
SkibstedN <niko933n@stud.kea.dk>
2022-03-14 12:50:04 +0100

    System Structure Draft 1.2.2

--- src/com/company/FamilyCar.java
+++ src/com/company/FamilyCar.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import com.company.Car;
+
 
 public class FamilyCar extends Car {
 
--- src/com/company/LuxuryCar.java
+++ src/com/company/LuxuryCar.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import com.company.Car;
+
 
 public class LuxuryCar extends Car {
 
