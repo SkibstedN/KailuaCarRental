d4534c6d36fe7668164b296804a35e32c93425dc
simonbang <simo12k1@stud.kea.dk>
2022-03-14 14:20:19 +0100

    Lavet Kailua Carrental print i Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	//menu(); oi oi222
+        System.out.println("| **    **          ****            **      **              **        **            ****");
+        System.out.println("| **  **           **  **           **      **              **        **           **  **");
+        System.out.println("| ***             **    **          **      **              **        **          **    **");
+        System.out.println("| **  **         **********         **      **               **      **          **********");
+        System.out.println("| **    **      **        **        **      **                **    **          **        **");
+        System.out.println("| **      **   **          **       **      **********          ****           **          **");
+        System.out.println();
+        System.out.println("|  ****         ***      ******        ******   ******  ****    **  ********    ***     **");
+        System.out.println("|**    **      ** **     **   **       **   **  **      ** **   **     **      ** **    **");
+        System.out.println("|**           *******    **  **        **  **   ******  **  **  **     **     *******   **");
+        System.out.println("|**    **    **     **   ** **         ** **    **      **   ** **     **    **     **  **");
+        System.out.println("|  ****     **       **  **   **       **   **  ******  **    ****     **   **       ** ******");
+
     }
 }
