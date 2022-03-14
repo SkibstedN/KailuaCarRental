4c481b442976132dbe651d3122fd4384574fed31
simonbang <simo12k1@stud.kea.dk>
2022-03-14 13:37:23 +0100

    Oprettet Fields i Contract klassen

--- src/com/company/FamilyCar.java
+++ src/com/company/FamilyCar.java
@@ -4,6 +4,7 @@ package com.company;
 
 public class FamilyCar extends Car {
 
+
     public FamilyCar(String brand, String model, String fuelType, int regNumber, String date, int odometer) {
         super(brand, model, fuelType, regNumber, date, odometer);
     }
