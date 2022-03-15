23b0c9ef1529115df6b1a3c748d9b67167741b51
simonbang <simo12k1@stud.kea.dk>
2022-03-15 15:15:48 +0100

    Oprettet og testet createCarType og testet med et forudfyldt objekt

--- src/com/company/ListContainer.java
+++ src/com/company/ListContainer.java
@@ -8,4 +8,7 @@ public class ListContainer {
     ArrayList<LuxuryCar> luxuryCars = new ArrayList<>();
     ArrayList<SportsCar> sportsCars = new ArrayList<>();
     ArrayList<Contract> contracts = new ArrayList<>();
+
+    // Jeg kan ikke få den til at bruge disse med mindre jeg sætter den ind som parameter til menu metoden i Menu klassen
+    // Vi skal genoverveje om vi skal have denne klasse
 }
